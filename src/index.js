840fb9f2fcae5c696e17068866f74f98b47d7a92
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-18 23:06:19 +0300

    update: reverse-init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const str = Math.abs(n).toString();
+    const reversedStr = str.split('').reverse().join('');
+    const reversedNum = Number(reversedStr);
+    return reversedNum;      
 }
